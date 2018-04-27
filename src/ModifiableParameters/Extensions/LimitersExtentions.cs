0b825a98cd209b002154508563ced257221ee208
Serovoy <alexander.serovoy@gmail.com>
2018-04-27 23:33:41 +0300

    added limiters extensions

---
+++ src/ModifiableParameters/Extensions/LimitersExtentions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ModifiableParameters.Limiters;
+using ModifiableParameters.Parameters;
+
+namespace ModifiableParameters.Extensions
+{
+    public static class LimitersExtentions
+    {
+        public static AParameterLimiter<V>[] RemoveAllLimiters<V>(this ILimitable<V> limitableParameter)
+        {
+            if (limitableParameter.LimitersCount == 0) return null;
+            var limiters = limitableParameter.GetLimiters().ToArray();
+            foreach (var limiter in limiters)
+            {
+                limitableParameter.RemoveLimiter(limiter);
+            }
+            return limiters;
+        }
+
+        public static void AddAllLimiters<V>(this ILimitable<V> limitableParameter, IEnumerable<AParameterLimiter<V>> limiters)
+        {
+            foreach (var limiter in limiters)
+            {
+                limitableParameter.AddLimiter(limiter);
+            }
+        }
+    }
+}
